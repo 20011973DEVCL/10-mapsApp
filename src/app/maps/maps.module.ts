d18c7523e45c3b0a2acdaa82b1c504cbbbbf2dca
Danilo Alarcon Lopez <danilo.alarcon.lopez@icloud.com>
2024-01-04 18:36:31 -0300

    Quede pillo en la seccion 21 video 285 pq hay que pagar el mapa

--- src/app/maps/maps.module.ts
+++ src/app/maps/maps.module.ts
@@ -1,10 +1,10 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
-import { MapsRoutingModule } from './maps-routing.module';
 import { MiniMapComponent } from './components/mini-map/mini-map.component';
 import { SideMenuComponent } from './components/side-menu/side-menu.component';
 import { MapsLayoutComponent } from './layout/maps-layout/maps-layout.component';
+import { MapsRoutingModule } from './maps-routing.module';
 import { FullScreenPageComponent } from './pages/full-screen-page/full-screen-page.component';
 import { MarkersPageComponent } from './pages/markers-page/markers-page.component';
 import { PropertiesPageComponent } from './pages/properties-page/properties-page.component';
